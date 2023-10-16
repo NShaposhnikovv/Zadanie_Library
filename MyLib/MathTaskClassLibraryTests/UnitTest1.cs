7a2073a834884924a7adb0a659549115fbe59263
NShaposhnikovv <99391815+@users.noreply.github.com>
2023-10-16 10:57:39 +0300

    add UnitTest

--- MyLib/MathTaskClassLibraryTests/UnitTest1.cs
+++ MyLib/MathTaskClassLibraryTests/UnitTest1.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathTaskClassLibrary;
 
 namespace MathTaskClassLibraryTests
 {
     [TestClass]
-    public class UnitTest1
+    public class TriangleTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RectangleArea_3and5_15returned()
         {
+            // исходные данные
+            int a = 3;
+            int b = 5;
+            int expected = 15;
+
+            // получение значения с помощью тестируемого метода
+            Triangle g = new Triangle();
+            int actual = g.RectangleArea(a, b);
+
+            // сравнение ожидаемого результата с полученным
+            Assert.AreEqual(expected, actual);
         }
     }
 }
